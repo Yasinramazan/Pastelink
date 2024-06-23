fcc3dec7071b458b9469a2f01d0d3d863a353e01
yasin lüleci <55895633+Yasinramazan@users.noreply.github.com>
2024-06-23 12:58:39 +0300

    firsto

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+843e65e9ad99ec91c1e74420bef07a28e4fab837")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e050b13659ce27e16af000692993dd64093667bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
